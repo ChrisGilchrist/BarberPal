1cd653c0f9ecae22453aec341dfc550547a229bd
Chris Gilchrist <chris@quix.ai>
2026-01-04 15:07:40 +0000

    update env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://ebllhdgqgddmplodhatx.supabase.co',
   supabaseKey: 'sb_publishable_NpYhWa_20N4UVhlIyN5Lig_WLWFsP4G',
-  vapidPublicKey: 'BJUHT-mwNwj7WrIjb4fXXCzrvReUCwbToXmDoXM0iGmWrrT0yEGncmnCyDjiYo5iNvn-slR2Ghm8nKXtOoKYUvY',
+  vapidPublicKey: 'BP5jzF4TtBHEuy0VAnlMbfvjFcdHQ5Geyw2MZSFAWojQuuQbotTxrZ9PLh7QQMokjPrR3a5a8Dm3subQ8XBEsJc',
 };
